1f7bc09cb2a133d4eb01c452c68273521c5482ff
Lubosz <lubosz@gmail.com>
2010-11-16 03:55:58 +0100

    updated test

--- test/BasicOgre.cpp
+++ test/BasicOgre.cpp
@@ -6,23 +6,31 @@
  *  Copyright 2010 The Collage Project
  */
 
-#include "CollageApplication.h"
-#include "Input.h"
-#include "RenderEngine.h"
-#include "UserInterface.h"
 #include "System.h"
-#include "MenuState.h"
-#include "GameState.h"
-#include "PauseState.h"
+
+using namespace Ogre;
 
 
 void basicOgre() {
 
-	RenderEngine::Instance().initOgre("Collage");
+	Ogre::Viewport* m_pViewport;
+	Ogre::Root* m_pRoot;
+	Ogre::RenderWindow* m_pRenderWnd;
+
+    m_pRoot = new Ogre::Root();
+    System::Instance().init();
+    m_pRoot->showConfigDialog();
+
+    m_pRenderWnd = m_pRoot->initialise(true, "Basic Ogre");
+    m_pRenderWnd->setActive(true);
+    m_pViewport = m_pRenderWnd->addViewport(0);
+    m_pViewport->setBackgroundColour(Ogre::ColourValue(0.5f, 0.5f, 0.5f, 1.0f));
+    m_pViewport->setCamera(0);
+	System::Instance().loadRecources();
 
 	Ogre::SceneManager* m_pSceneMgr;
 	Ogre::Camera* m_pCamera;
-	m_pSceneMgr = RenderEngine::Instance().m_pRoot->createSceneManager(
+	m_pSceneMgr = m_pRoot->createSceneManager(
 			Ogre::ST_GENERIC, "GameSceneMgr");
 	m_pSceneMgr->setAmbientLight(Ogre::ColourValue(0.7f, 0.7f, 0.7f));
 
@@ -32,45 +40,29 @@ void basicOgre() {
 	m_pCamera->setNearClipDistance(5);
 
 	m_pCamera->setAspectRatio(Real(
-			RenderEngine::Instance().m_pViewport->getActualWidth()) / Real(
-			RenderEngine::Instance().m_pViewport->getActualHeight()));
+			m_pViewport->getActualWidth()) / Real(
+			m_pViewport->getActualHeight()));
 
-	RenderEngine::Instance().m_pViewport->setCamera(m_pCamera);
+	m_pViewport->setCamera(m_pCamera);
 
 	Ogre::SceneNode* m_pOgreHeadNode;
 	Ogre::Entity* m_pOgreHeadEntity;
-	Ogre::MaterialPtr m_pOgreHeadMat;
-	Ogre::MaterialPtr m_pOgreHeadMatHigh;
 
 	m_pOgreHeadEntity = m_pSceneMgr->createEntity("Cube", "ogrehead.mesh");
-	m_pOgreHeadEntity->setQueryFlags(OGRE_HEAD_MASK);
-	m_pOgreHeadNode = m_pSceneMgr->getRootSceneNode()->createChildSceneNode(
-			"CubeNode");
+	m_pOgreHeadNode = m_pSceneMgr->getRootSceneNode()->createChildSceneNode("CubeNode");
 	m_pOgreHeadNode->attachObject(m_pOgreHeadEntity);
 	m_pOgreHeadNode->setPosition(Vector3(0, 0, -25));
-
-	m_pOgreHeadMat = m_pOgreHeadEntity->getSubEntity(1)->getMaterial();
-	m_pOgreHeadMatHigh = m_pOgreHeadMat->clone("OgreHeadMatHigh");
-	m_pOgreHeadMatHigh->getTechnique(0)->getPass(0)->setAmbient(1, 0, 0);
-	m_pOgreHeadMatHigh->getTechnique(0)->getPass(0)->setDiffuse(1, 0, 0, 0);
 
 	int timeSinceLastFrame = 1;
 	int startTime = 0;
 
-	int rotation =0;
-
 	while (true) {
 		startTime = System::Instance().m_pTimer->getMillisecondsCPU();
+		m_pOgreHeadNode->rotate(Ogre::Vector3(0,1,0), Ogre::Radian(float(startTime%10000)/100000.0));
+		m_pRoot->renderOneFrame();
 
-		m_pOgreHeadNode->rotate(Ogre::Vector3(0,1,0), Ogre::Radian(rotation));
+		timeSinceLastFrame = System::Instance().m_pTimer->getMillisecondsCPU() - startTime;
 
-		RenderEngine::Instance().updateOgre(timeSinceLastFrame);
-		RenderEngine::Instance().m_pRoot->renderOneFrame();
-
-		timeSinceLastFrame = System::Instance().m_pTimer->getMillisecondsCPU()
-				- startTime;
-
-		rotation++;
 	}
 
 
