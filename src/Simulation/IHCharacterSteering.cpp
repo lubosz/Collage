f5f7b0981ced2eb722993b24226ce3564cddec95
Christopher Denter <christopher@pymt.eu>
2011-01-17 18:01:29 +0100

    missing files

---
+++ src/Simulation/IHCharacterSteering.cpp
@@ -0,0 +1,19 @@
+/*
+ * LevelState.cpp
+ *
+ *  Created on: Nov 14, 2010
+ *  Author: deine mudda
+ *  Copyright 2010 The Collage Project
+ */
+
+#include <iostream>
+#include "IHCharacterSteering.h"
+
+
+IHCharacterSteering::IHCharacterSteering() {
+}
+
+void IHCharacterSteering::inside(Interaction* interaction, float d_t) {
+  std::cout << "basdasdasdasdasdasdasdasdasdasdla" << std::endl;
+  // interaction->getA()->addVelocity(Ogre::Vector3 );
+}
