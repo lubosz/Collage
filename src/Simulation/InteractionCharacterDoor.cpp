511440bc35554f72f598e9d6a0a8de9ee1ecc86a
Christopher Denter <christopher@kivy.org>
2011-06-15 12:37:48 +0200

    print something if we stand in front of a door and press space

--- src/Simulation/InteractionCharacterDoor.cpp
+++ src/Simulation/InteractionCharacterDoor.cpp
@@ -2,9 +2,21 @@
  *  Copyright 2011 The Collage Project
  */
 
+#include "Input.h"
+#include "CollisionHandler.h"
 #include "InteractionCharacterDoor.h"
 
 bool Interaction<Character, Door>::collisionTest() {
+  Ogre::Vector2 collisionNormal = Ogre::Vector2::ZERO;
+  if (CollisionHandler::collisionTestAABB(
+      &first->collisionShape,
+      &second->collisionShape,
+      &first->moveVector,
+      &first->possibleMoveRatio,
+      &collisionNormal)) {
+    return true;
+  }
+  return false;
 }
 
 void Interaction<Character, Door>::onInit() {}
@@ -13,6 +25,10 @@ void Interaction<Character, Door>::onEnter() {}
 
 void Interaction<Character, Door>::onLeave() {}
 
-void Interaction<Character, Door>::whileInside(float d_t) {}
+void Interaction<Character, Door>::whileInside(float d_t) {
+  if (Input::Instance().m_pKeyboard->isKeyDown(OIS::KC_SPACE)) {
+	qDebug() << "Standing in front of door!";
+  }
+}
 
 void Interaction<Character, Door>::whileOutside(float d_t) {}
